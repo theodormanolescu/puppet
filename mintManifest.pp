68ac3e37fa0adbbe50f9465c37ece7b93270c7de
Theodor Manolescu <manolescu.theodor@yahoo.com>
2016-04-12 10:24:00 +0300

    Update mintManifest.pp
    run apt-update once per day

--- mintManifest.pp
+++ mintManifest.pp
@@ -1,6 +1,11 @@
+schedule { 'everyday':
+  period => daily,
+  range  => "2 - 4",
+}
 # execute 'apt-get update'
 exec { 'apt-update':                    # exec resource named 'apt-update'
-  command => '/usr/bin/apt-get update'  # command this resource will run
+  command => '/usr/bin/apt-get update',  # command this resource will run
+  schedule => 'everyday'
 }
 
 $applications = [ 'vim', 'chromium-browser', 'htop', 'git' ]
